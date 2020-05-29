40b88506e4c1220574a2ca8e60710459febf2940
Andrii Kopylov <akopy@softserveinc.com>
2020-05-29 23:13:16 +0300

    Added pagination

--- src/models/UserListModel.js
+++ src/models/UserListModel.js
@@ -1,16 +1,19 @@
-import {renderUserList} from "../views/UserListView";
+import {renderPagination, renderUserList} from "../views/UserListView";
 
-let url ='https://reqres.in/api/users/';
+const url ='https://reqres.in/api/users';
 let userListObj;
 export async function getUserList(parametrs = '') {
+    let updatedUrl = url
     if (parametrs) {
         //update url w/params
+        updatedUrl += parametrs;
     }
 
-    let response = await fetch(url);
-    if (response.ok && response.status == 200){
+    let response = await fetch(updatedUrl);
+    if (response.ok && response.status === 200){
         userListObj = await response.json();
         renderUserList(userListObj);
+        renderPagination(userListObj.page, userListObj.total_pages, !!(parametrs))
     }
 }
 
--- src/views/UserListView.js
+++ src/views/UserListView.js
@@ -1,22 +1,23 @@
+import {getUserList} from "../models/UserListModel";
+
 const viewDataStructure = [
     ['last_name', 'text'],
     ['first_name', 'text'],
     ['email', 'text'],
     ['avatar', 'img'],
 ]
+const container = document.body.querySelector('.container');
+
 export function renderUserList(userListObj) {
     const userList = userListObj.data;
-    const container = document.body.querySelector('.container');
+
+    clearUserList();
     let listHTMLView = [];
     for (let userObj of userList){
         let userContainer = document.createElement('div');
         userContainer.className = 'user_container';
-
         renderUser(userObj,userContainer);
-
-        console.log( userContainer);
         listHTMLView.push(userContainer);
-
     }
     container.append(...listHTMLView);
 }
@@ -30,16 +31,44 @@ function renderUser(userObj, userContainer){
         let userDiv = document.createElement('div');
         userDiv.className = 'user';
         userDiv.id = key;
-        if(type == 'img'){
+        if(type ===  'img'){
             let userImg = document.createElement('img');
             userImg.src = userObj[key];
             userImg.alt = '';
-            console.log(userImg);
-            userDiv.append(...[userImg,]);
+            userDiv.append(...[userImg]);
         }else {
             userDiv.innerHTML = userObj[key];
         }
         userHTMLView.push(userDiv);
     }
     userContainer.append(...userHTMLView);
+}
+
+export function renderPagination(curr_page, total_pages, isUpdating = false){
+    const pages = document.body.querySelectorAll('.pagination > li');
+    for(let i=0; i<pages.length; i++){
+        let page = pages[i];
+        page.classList.remove('active','inactive');
+        if(i >= total_pages){
+            page.classList.add('inactive');
+        }
+        if(curr_page === i+1){
+            page.classList.add('active');
+        }
+        // if(!isUpdating) {
+            page.addEventListener('click', () => {
+                changePage(i + 1)
+            })
+        // }
+    }
+}
+
+function clearUserList(){
+    const usersList = document.body.querySelectorAll('.container > .user_container:not(.list_header)')
+    for(let user of usersList){
+        user.remove();
+    }
+}
+function changePage(pageNumber){
+    getUserList('?page=' + pageNumber);
 }
